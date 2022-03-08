81daef693e1bc906904d07a3dbfded04f147134c
JulijsO <77743919+JulijsO@users.noreply.github.com>
2022-03-08 16:27:24 +0100

    Add files via upload

---
+++ scripts/projectList.js
@@ -0,0 +1,62 @@
+$(document.body).on("click", ".clickable-pl",function(){
+
+    $(".list-container").addClass("list-container-on");
+
+    $(".list-padding").addClass("list-padding-on");
+
+    $(".projectlist-child").addClass("projectlist-child-on");
+
+    // $(this).addClass("projectlist-on");
+
+    $(".projectlist-title").addClass("projectlist-title-on");
+
+    $(".closed-list").addClass("close-list");
+
+    $(".triangle").css("display", "none");
+
+});
+
+$(document.body).on("click", ".close-list",function(){
+
+    $(".list-container").removeClass("list-container-on");
+
+    $(".list-padding").removeClass("list-padding-on");
+
+    $(".projectlist-child").removeClass("projectlist-child-on");
+
+    // $(this).toggleClass("projectlist-on");
+
+    $(".projectlist-title").removeClass("projectlist-title-on");
+
+    $(this).removeClass("close-list");
+
+    $(".triangle").css("display", "unset");
+
+});
+
+
+$(document.body).on("click", ".logo-title",function(){
+
+    // $(".list-container").removeClass("list-container-on");
+
+    // $(".list-padding").removeClass("list-padding-on");
+
+    // $(".projectlist-child").removeClass("projectlist-child-on");
+
+    // // $(this).toggleClass("projectlist-on");
+
+    // $(".projectlist-title").removeClass("projectlist-title-on");
+
+    $(".close-list").removeClass("close-list");
+
+    $(".triangle").css("display", "unset");
+
+});
+
+$(document.body).hover(".clickable-pl",function(){
+
+    $(".triangle").css("color", "blue");
+
+});
+
+
